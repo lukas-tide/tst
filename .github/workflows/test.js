6a85ef544a777fb43f3620630ac6401671f89f1a
Lukas Ignatavičius <132355146+lukas-tide@users.noreply.github.com>
2024-07-16 14:31:38 +0000

    debug

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -2,7 +2,7 @@ module.exports = async ({ github, context, core }) => {
     for (let i in arguments) {
         a = arguments[i]
         console.log(`Argument ${i}`)
-        console.debug(i)
+        console.debug(a)
         console.log()
         console.log()
     }
